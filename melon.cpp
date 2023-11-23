a98daa490e72619604e11d84eff85d51ab785f71
yourd8ne <done101@mail.ru>
2023-11-23 11:04:07 +0300

    23.11

---
+++ melon.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    string input;
+    getline(cin, input);
+
+    stringstream ss(input);
+    int num;
+    vector<int> arr;
+    
+    while(ss >> num){
+        arr.push_back(num);
+    }
+
+    int min = arr[0];
+    int max = arr[0];
+
+    for(int i = 0; i < N; ++i) {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    cout << min <<  ' ' << max << endl;
+    return 0;
+}
